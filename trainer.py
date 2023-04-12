8f32a212bd7afe67d10798440dd26ed36444e717
Junhui Li <54486461+sleepyMMp@users.noreply.github.com>
2023-04-12 23:22:48 +0800

    trainer

--- trainer.py
+++ trainer.py
@@ -64,3 +64,90 @@ args: Args = hf_parser.parse_yaml_file(cli_args.config_path)[0]
 pprint(args.__dict__)
 
 # %%
+# train_inputs, train_targets = dataset.dataset()
+from datasets import load_dataset
+data = load_dataset("opus100", "en-zh")
+
+
+# %% model
+tokenizer = AutoTokenizer.from_pretrained(args.tokenizer_path)
+tokenizer.max_length = args.max_length
+
+if args.model_class_name == 'auto':
+    model = AutoModelForCausalLM.from_pretrained(args.model_path)
+elif args.model_class_name == 'flash opt':
+    from models import OPTForCausalLM
+
+    model = OPTForCausalLM.from_pretrained(args.model_path)
+else:
+    raise ValueError(f'Unknown model class name: {args.model_class_name}')
+
+data_collator = DataCollatorForLanguageModeling(tokenizer=tokenizer, mlm=False)
+
+# %% trainer
+train_args = TrainingArguments(
+    output_dir=args.output_dir,
+    overwrite_output_dir=False,
+
+    # data
+    per_device_train_batch_size=args.mini_batch_size,
+    per_device_eval_batch_size=args.mini_batch_size,
+    gradient_accumulation_steps=args.gradient_accumulation_steps,
+    dataloader_num_workers=2,
+    gradient_checkpointing=args.gradient_checkpointing,
+
+    # precision
+    bf16=True,
+    bf16_full_eval=True,
+    tf32=True,
+    fp16_opt_level=args.fp16_opt_level,
+
+    # eval
+    evaluation_strategy='steps',
+    save_strategy='steps',
+    eval_steps=100,
+    save_steps=100,
+    save_total_limit=1,
+    metric_for_best_model='eval_loss',
+    prediction_loss_only=True,
+
+    # log
+    logging_strategy='steps',
+    logging_steps=10,
+    logging_dir=args.output_dir,
+    logging_first_step=True,
+    run_name=args.run_name,
+    report_to=['wandb'],
+
+    # hyper-parameters
+    learning_rate=args.learning_rate,
+    lr_scheduler_type='constant',
+    num_train_epochs=args.num_train_epochs,
+    warmup_steps=0,
+    weight_decay=0.0,
+    optim=args.optim,
+
+    greater_is_better=False,
+
+    # flags
+    do_train=True,
+    do_eval=True,
+    do_predict=False,
+    load_best_model_at_end=True,
+    torch_compile=args.torch_compile,
+    seed=42,
+)
+
+trainer = Trainer(
+    args=train_args,
+    model=model,
+    data_collator=data_collator,
+    train_dataset=data['train'],
+    eval_dataset=data['test'],
+
+)
+
+
+trainer.train()
+trainer.save_model(output_dir=f'{args.output_dir}/best')
+tokenizer.save_pretrained(f'{args.output_dir}/best')
